1de730207aa56d9865c4d15e7d2564454d605b0a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-26 19:25:35 +0530

    add activities

--- Activities/questions.sql
+++ Activities/questions.sql
@@ -1,10 +1,11 @@
 -- Write a SQL statement to display a string "This is SQL Exercise". 
 -- Write a SQL query to display three numbers in three columns. 
---  Write a SQL query to display the sum of two numbers 10 and 15
---  write a query to evaluate any arithmetic  expression
---  display all the columns and rows of emp table
+-- Write a SQL query to display the sum of two numbers 10 and 15
+-- write a query to evaluate any arithmetic  expression (10+20*70+20/10)
 
-/* Usinf emp schema */
+
+/* Using emp schema */
 --  display all employee names 
+--  display all the columns and rows of emp table
 --  display employees working in department number 10
 --  display all the columns and rows of emp table where salary is more than 2500
