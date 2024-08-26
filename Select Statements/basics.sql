91bd83f41999e99036e612960dc7050fc7a5ed21
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-26 17:51:38 +0530

    add basic select statements

---
+++ Select Statements/basics.sql
@@ -0,0 +1,15 @@
+-- command to list the databases in the connection.
+show databases;
+
+-- use a particular database
+use emp;
+
+-- display tables in current schema
+show tables;
+
+-- display the structure of the table
+-- metadata of the table
+desc emp;
+
+-- display the version of MySql server
+select version();
