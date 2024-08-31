3ff7f4e64e5f473f141b78c5aa9c4c9dc9357e9a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-31 14:33:33 +0530

    add group functions

---
+++ Select Statements/groupby clause/groupby.sql
@@ -0,0 +1,22 @@
+/*
+The GROUP BY clause in MySQL is used to group rows that have the same values in specified columns into aggregated data. 
+It's commonly used in conjunction with aggregate functions like SUM(), COUNT(), AVG(), MAX(), and MIN() 
+to perform calculations on each group of rows.
+Syntax
+SELECT column1, column2, ..., aggregate_function(column)
+FROM table
+WHERE condition
+GROUP BY column1, column2, ...;
+
+	column1, column2, ...`: These are the columns by which you want to group the data.
+	aggregate_function(column): This is an aggregate function applied to each group.
+	table: The table from which to retrieve the data.
+	condition: (Optional) This is a condition to filter the rows before grouping them.
+    
+*/
+
+SELECT 
+    deptno, COUNT(sal), SUM(sal), AVG(sal), MAX(sal), MIN(sal)
+FROM
+    emp
+GROUP BY deptno;
