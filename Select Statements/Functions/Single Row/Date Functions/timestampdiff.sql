3076a6a749b2df3313d0c043a49f570f4791c409
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-31 11:56:21 +0530

    add Date functions

---
+++ Select Statements/Functions/Single Row/Date Functions/timestampdiff.sql
@@ -0,0 +1,6 @@
+
+select timestampdiff(day, curdate(), hiredate) from emp.emp;
+
+select timestampdiff(month, curdate(), hiredate) from emp.emp;
+
+select timestampdiff(year, curdate(), hiredate) from emp.emp;
