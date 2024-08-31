8db8d4274625f6a5d00f0e469d4d98d9e88b1593
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-31 09:26:03 +0530

    add Date functions

---
+++ Select Statements/Functions/Single Row/Date Functions/default data formats.sql
@@ -0,0 +1,14 @@
+/*
+Date:  YYYY-MM-DD
+Example: 2024-08-31
+
+Datetime: YYYY-MM-DD HH:MM:SS
+Example: 2024-08-31 09:23:55
+ Note: If the time is not specified, it defaults to 00:00:00
+
+Time: HH:MM:SS
+Example: 09:23:55
+
+Year: YYYY
+Example: 2024
+*/
