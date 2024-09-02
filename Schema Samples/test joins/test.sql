00937a03f8d269833620d994cc56bf3ac0f2198a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-02 19:16:15 +0530

    add joins

--- Schema Samples/test joins/test.sql
+++ Schema Samples/test joins/test.sql
@@ -13,6 +13,8 @@ insert into t1 values(10);
 insert into t1 values(null);
 
 insert into t2 values(1);
+insert into t2 values(14);
+insert into t2 values(15);
 insert into t2 values(3);
 insert into t2 values(7);
 insert into t2 values(10);
