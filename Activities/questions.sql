17a333e9b4bb88ecc0d0db124597160e5f7d9274
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-03 18:56:57 +0530

    add joins on activities

--- Activities/questions.sql
+++ Activities/questions.sql
@@ -48,6 +48,7 @@
 
 
 -- Group By clause using the Northwind database
+--------------------------------------------------------------
 -- Total Number of Orders per Customer
 -- Total Number of Orders by Each Employee
 -- Average Freight Cost per Shipper
@@ -59,6 +60,34 @@
 -- Number of Orders Handled by Each Employee
 -- Number of Products Supplied by Each Supplier
 
+-- JOINS on HR Database
+------------------------------
+-- find the first name, last name, department number, and department name for each employee.
+-- find the first name, last name, department, city, and state province for each employee.
+-- find the first name, last name, salary, and job grade for all employees.
+-- find all those employees who work in department ID 80 or 40. Return first name, last name, department number and department name.
+-- find those employees whose first name contains the letter ‘z’. Return first name, last name, department, city, and state province.
+-- find all departments, including those without employees. Return first name, last name, department ID, department name.
+-- find the employees who earn less than the employee of ID 182. Return first name, last name and salary.
+-- find the employees and their managers. Return the first name of the employee and manager.
+-- display the department name, city, and state province for each department.
+-- find out which employees have or do not have a department. Return first name, last name, department ID, department name.
+-- find the employees and their managers. Those managers do not work under any manager also appear in the list. Return the first name of the employee and manager.
+-- find the employees who work in the same department as the employee with the last name Taylor. Return first name, last name and department ID.
+-- find all employees who joined on or after 1st January 1993 and on or before 31 August 1997. Return job title, department name, employee name, and joining date of the job.
+-- calculate the difference between the maximum salary of the job and the employee's salary. Return job title, employee name, and salary difference.
+-- calculate the average salary, the number of employees receiving commissions in that department. Return department name, average salary and number of employees.
+-- calculate the difference between the maximum salary and the salary of all the employees who work in the department of ID 80. Return job title, employee name and salary difference.
+-- find the name of the country, city, and departments, which are operational in those cities.
+-- find the department name and the full name (first and last name) of the manager.
+-- calculate the average salary of employees for each job title.
+-- find the employees who earn $12000 or more. Return employee ID, starting date, end date, job ID and department ID.
+-- find out which departments have at least two employees. Group the result set on country name and city.
+-- calculate the number of days worked by employees in a department of ID 80
+-- find full name (first and last name), and salary of all employees working in any department in the city of London.
+-- find full name (first and last name), job title, start and end date of last jobs of employees who do not receive commissions.
+-- find the department name, department ID, and number of employees in each department.
+-- find out the full name (first and last name) of the employee with an ID and the name of the country where he/she is currently employed.
 
 
 
