8fd99428c52c7cce5ad5e63cb717e1c927782576
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-07 08:48:58 +0530

    add sub queries

--- Select Statements/Sub Queries/from clause.sql
+++ Select Statements/Sub Queries/from clause.sql
@@ -8,3 +8,29 @@ FROM
     GROUP BY deptno
     ORDER BY 1
     LIMIT 1) AS e;
+    
+    
+    SELECT 
+    x.deptno
+FROM
+    (SELECT 
+        deptno
+    FROM
+        emp
+    GROUP BY deptno
+    HAVING COUNT(deptno) < 6) AS x;
+    
+SELECT 
+    e1.ename, e1.sal, e1.deptno, asal.avg_sal
+FROM
+    emp e1
+        JOIN
+    (SELECT 
+        deptno, AVG(sal) AS avg_sal
+    FROM
+        emp
+    GROUP BY deptno) AS asal ON e1.deptno = asal.deptno
+WHERE
+    e1.sal > asal.avg_sal
+ORDER BY 3
+;
