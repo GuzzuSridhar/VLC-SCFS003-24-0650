5e19236e3f2b31acbb6b19d41347d9fde28a97dd
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-09 19:09:15 +0530

    add DDL

--- DDL/Create/Table/Constraints/unique.sql
+++ DDL/Create/Table/Constraints/unique.sql
@@ -26,7 +26,6 @@ CREATE TABLE IF NOT EXISTS test_unique_column (
     first_name VARCHAR(255) unique, -- column level constraint
     last_name VARCHAR(255)
     );
-
     
     insert into test_unique_column values (1,'david', 'boon');
     insert into test_unique_column values (2,'david', 'alex'); -- error as the first_name is repeated
@@ -43,4 +42,13 @@ CREATE TABLE IF NOT EXISTS test_unique_column (
     employee_id INT,
     first_name VARCHAR(255) not null unique, -- column level constraint (multiple)
     last_name VARCHAR(255)
+    );
+    
+    
+    -- custom name to the constraint
+        CREATE TABLE IF NOT EXISTS test_custom_constraint_name (
+    employee_id INT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    unique key my_Constraint (first_name,last_name) -- table level composite constraint with a custom name
     );
