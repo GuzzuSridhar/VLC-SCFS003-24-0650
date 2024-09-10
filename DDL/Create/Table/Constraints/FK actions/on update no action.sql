7470e302d5d7735a192a224eb5cc3ad7d90e8fb1
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-10 17:58:06 +0530

    add constraints

---
+++ DDL/Create/Table/Constraints/FK actions/on update no action.sql
@@ -0,0 +1,45 @@
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT,
+    department_name VARCHAR(255),
+    PRIMARY KEY (department_id)
+);
+
+CREATE TABLE if not exists employees (
+    employee_id INT AUTO_INCREMENT,
+    name VARCHAR(255),
+    department_id INT,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+    ON UPDATE no action
+);
+
+-- Insert into if not exists departments
+INSERT INTO departments (department_name) 
+VALUES 
+('Human Resources'),
+('Finance'),
+('Marketing'),
+('Sales'),
+('IT');
+
+-- Insert into employees
+INSERT INTO employees (name, department_id) 
+VALUES 
+('Alice Johnson', 1),  
+('Bob Smith', 2),      
+('Charlie Davis', 3),  
+('David Williams', 4), 
+('Emily Clark', 5);  
+
+-- Display records updating a  update department
+SELECT * FROM departments;
+SELECT * FROM employees;
+
+-- Update department_id for Sales department (Not Allowed)
+UPDATE departments 
+SET department_id = 10 
+WHERE department_id = 4;
+
+-- Display records after updating  department
+SELECT * FROM departments;
+SELECT * FROM employees;
