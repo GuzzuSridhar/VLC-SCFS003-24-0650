3dddbb98da9ead21d6d2269c1429e819045e3a29
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-10 17:22:21 +0530

    add constraints

--- update.sql
+++ update.sql
@@ -11,3 +11,12 @@ insert into emp_dml values(4,'c',200,300);
 insert into emp_dml values(5,'e',200,300); 
 insert into emp_dml values(6,'f',200,300); 
 insert into emp_dml values(4,'g',200,400); 
+
+-- update all records
+update emp_dml set ename = 'h';
+
+-- update contionally
+update emp_dml set ename = 'a' where empid = 10;
+
+-- delete a record
+delete from emp_dml where department = 400;
