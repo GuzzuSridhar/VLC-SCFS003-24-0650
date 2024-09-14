cbd51e3cda39c60856baec9a00628859e54a3afd
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-14 11:20:05 +0530

    add alter

--- DDL/Alter/drop constraint.sql
+++ DDL/Alter/drop constraint.sql
@@ -8,7 +8,7 @@ Syntax
 	ALTER TABLE table_name
 		DROP PRIMARY KEY;
 	ALTER TABLE table_name
-		DROP INDEX constraint_name;
+		DROP INDEX constraint_name; (for unique constraint)
 	ALTER TABLE table_name
 		DROP FOREIGN KEY constraint_name;
 	ALTER TABLE table_name
@@ -16,3 +16,16 @@ Syntax
 	ALTER TABLE table_name
 		DROP CHECK constraint_name;
 */
+
+-- example of dropping a primary key with an Auto_increment
+ alter table employees drop primary key; -- will show an error
+  alter table employees modify employee_id int; -- removing the auto_increment
+  alter table employees drop primary key; -- success
+  
+  -- drop unique constraint
+  alter table employees drop INDEX email;
+  
+  -- check the ddl of the table
+  show create table employees;
+  
+  
