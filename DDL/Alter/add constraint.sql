12fc4b1dac543760c19402b3ddbce3a73a03cfd7
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-14 09:20:15 +0530

    add alter

--- DDL/Alter/add constraint.sql
+++ DDL/Alter/add constraint.sql
@@ -3,6 +3,22 @@
     Syntax
     ALTER TABLE table_name
 			ADD CONSTRAINT constraint_name PRIMARY KEY (column_name);
+            
+	ALTER TABLE child_table
+			ADD CONSTRAINT fk_constraint_name FOREIGN KEY (column_name)
+			REFERENCES parent_table (parent_column)
+			ON DELETE action
+			ON UPDATE action;
+	
+    ALTER TABLE table_name
+			ADD CONSTRAINT constraint_name CHECK (condition);
+            
+	ALTER TABLE table_name
+			ADD CONSTRAINT constraint_name UNIQUE (column_name);
+            
+	For column level constraints
+			ALTER TABLE table_name
+			MODIFY COLUMN column_name data_type NOT NULL;
 
 */
 
