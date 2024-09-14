f7a33e926f33e04da50b96c8217687b410523bac
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-14 08:35:33 +0530

    add alter

---
+++ DDL/Alter/syntax.sql
@@ -0,0 +1,15 @@
+/*
+The ALTER TABLE statement in SQL is used to modify an existing table's structure. 
+This can include adding, deleting, or modifying columns, adding constraints, or changing the table's name.
+Syntax
+ALTER TABLE table_name
+    action;
+    
+    Common Actions:
+    ADD: Adds new columns or constraints.
+	DROP: Removes columns or constraints.
+	MODIFY: Changes the data type or constraints of existing columns.
+	RENAME: Changes column names or table names.
+	ADD CONSTRAINT: Adds new constraints to the table.
+	DROP CONSTRAINT: Removes existing constraints.
+*/
