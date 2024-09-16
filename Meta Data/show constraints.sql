add0f2a5f3c7400eb64d14f2f8bfcd7051c63ee9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-16 17:34:37 +0530

    add meta data

--- Meta Data/show constraints.sql
+++ Meta Data/show constraints.sql
@@ -1,3 +1,4 @@
+-- display all constraints from a a given table
 SELECT 
     tc.CONSTRAINT_NAME,
     tc.CONSTRAINT_TYPE,
@@ -14,3 +15,27 @@ WHERE
     AND tc.TABLE_SCHEMA = 'emp'
 ORDER BY 
     tc.CONSTRAINT_NAME, kcu.ORDINAL_POSITION;
+    
+    -- show primary key
+SELECT * 
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE TABLE_NAME = 'table_name' 
+AND CONSTRAINT_TYPE = 'PRIMARY KEY';
+
+-- show unique constraints
+SELECT * 
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE TABLE_NAME = 'table_name' 
+AND CONSTRAINT_TYPE = 'UNIQUE';
+
+-- show Foreign Key Constraints
+SELECT * 
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'table_name'
+AND REFERENCED_TABLE_NAME IS NOT NULL;
+
+-- show Check Constraints
+SELECT * 
+FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS
+WHERE CONSTRAINT_SCHEMA = 'database_name'
+AND TABLE_NAME = 'table_name';
